8812a33b1ef5b9799fcf252799d736d9264ed5c1
LeeKennedy <mail@leekennedy.id.au>
2015-04-02 22:04:11 +1100

    March adjustments

--- weather.yearonyear.dplyr.R
+++ weather.yearonyear.dplyr.R
@@ -6,22 +6,22 @@ library(reshape2)
 melb <- na.omit(melb)
 
 max.temp1 <- select(melb, Year:Min)%>%
-  filter(Year == 2013 )%>%
+  filter(Year == 2014 )%>%
   group_by(Month)%>%
   summarize(Ave = mean(Max))
 
 barplot(max.temp1$Ave)
 
 max.temp2 <- select(melb, Year:Min)%>%
-  filter(Year == 2014 )%>%
+  filter(Year == 2015 )%>%
   group_by(Month)%>%
   summarize(Ave = mean(Max))
 
 barplot(max.temp2$Ave)
 
 CompYear <- cbind(max.temp1, max.temp2)
-colnames(CompYear)[2] <- c("Y2012")
-colnames(CompYear)[4] <- c("Y2013")
+colnames(CompYear)[2] <- c("Y2014")
+colnames(CompYear)[4] <- c("Y2015")
 CompYear <- CompYear[,c(1,2,4)]
 
 CY <- melt(CompYear, id=c("Month"))
