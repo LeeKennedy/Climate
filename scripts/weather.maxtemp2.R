d561dc99bee50748bd3ba1c9fa8b26528c5ef009
LeeKennedy <mail@leekennedy.id.au>
2018-08-04 07:48:31 +1000

    misc twiddling.

--- scripts/weather.maxtemp2.R
+++ scripts/weather.maxtemp2.R
@@ -8,7 +8,7 @@ library(reshape2)
 dataw <- read.csv("data/melbourne.csv", header = TRUE)
 
 input <- dataw %>%
-        filter(Month == 5) %>%
+        filter(Month == 7) %>%
         group_by(Year) %>%
         summarise(Max_month = max(Max))
 
