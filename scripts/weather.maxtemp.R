9f9a9daaa9e6f5ff417d10afcefc3480c39e6137
LeeKennedy <mail@leekennedy.id.au>
2019-01-24 22:13:58 +1100

    prettier

--- scripts/weather.maxtemp.R
+++ scripts/weather.maxtemp.R
@@ -1,22 +1,23 @@
-library('ProjectTemplate')
-load.project()
+library(tidyverse)
 
 # Calculates the number of days above a given maximum temperature--------------------
 
 dataw <- read.csv("data/melbourne.csv", header = TRUE)
 
 input <- dataw %>%
-        filter(Month == 4) %>%
-        filter (Max >= 30 ) %>%
         group_by(Year) %>%
+        filter(Month == 1) %>%
+        filter (Max >= 42 ) %>%
         summarise(Count = n())
         
 input
 
-plot(input$Count)
-
 plot <- ggplot(input, aes(x=Year, y = Count)) +
-        geom_point(size=4, shape=21, colour = "darkgreen") +
-        geom_smooth(method=loess)
+        geom_point(size=4, shape=21, fill = "cornflowerblue", colour = "darkgreen") +
+        geom_smooth(method=loess)+
+        theme_bw() +
+        theme(panel.grid.major = element_line(size = 0.5, color = "grey"), 
+        axis.line = element_line(size = 0.7, color = "black"), 
+        text = element_text(size = 14))
 plot
 
