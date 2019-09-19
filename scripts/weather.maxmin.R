740debce80cdc87940d33ab09c3aae21540eba1c
LeeKennedy <mail@leekennedy.id.au>
2019-09-20 09:31:54 +1000

    Max Min

---
+++ scripts/weather.maxmin.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+# Calculates the number of days above a given maximum temperature--------------------
+
+dataw <- read.csv("data/melbourne.csv", header = TRUE)
+
+input <- dataw %>%
+        group_by(Year) %>%
+        filter(Month == 9) %>%
+        #filter (Min >= 18 ) %>%
+        summarise(Count = n(), Max = max(Min))
+        
+input
+
+plot <- ggplot(input, aes(x=Year, y = Max)) +
+        geom_point(size=4, shape=21, fill = "cornflowerblue", colour = "darkgreen") +
+        geom_smooth(method=loess)+
+        theme_bw() +
+        theme(panel.grid.major = element_line(size = 0.5, color = "grey"), 
+        axis.line = element_line(size = 0.7, color = "black"), 
+        text = element_text(size = 14))
+plot
+
