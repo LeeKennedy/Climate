af66349ec8e925dc8012e509b5f98b3c9724bd13
LeeKennedy <mail@leekennedy.id.au>
2020-04-01 17:57:18 +1100

    playing

--- scripts/weather.maxmin.R
+++ scripts/weather.maxmin.R
@@ -6,7 +6,7 @@ dataw <- read.csv("data/melbourne.csv", header = TRUE)
 
 input <- dataw %>%
         group_by(Year) %>%
-        filter(Month == 2) %>%
+        filter(Month == 3) %>%
         #filter (Min >= 18 ) %>%
         summarise(Count = n(), Max = max(Min))
         
